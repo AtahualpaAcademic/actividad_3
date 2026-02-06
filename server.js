ddcca26a4cf9450d6cf943cf6d78b714f8bd6844
Santiago Atahualpa Meléndez Torres <al07041041@tecmilenio.mx>
2026-02-05 19:26:58 -0600

    se crea el archivo server.js

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = 3000;
+
+// Middleware para leer JSON
+app.use(bodyParser.json());
+
+// Middleware de errores 
+const errorHandler = require('./middleware/errorHandler');
+
+// Rutas
+const tareasRoutes = require('./routes/tareas');
+const authRoutes = require('./routes/auth');
+
+app.use('/api', authRoutes);
+app.use('/api/tareas', tareasRoutes);
+
+// Ruta no encontrada
+app.use((req, res) => {
+  res.status(404).json({ mensaje: 'Ruta no encontrada' });
+});
+
+// Middleware de errores
+app.use(errorHandler);
+
+// Levantar servidor
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
